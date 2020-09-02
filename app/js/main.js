64654c53f6c1f9ae383920d522474652fd0a5d70
mizza-cz <mizza.cz666@gmail.com>
2020-09-02 17:10:23 +0200

    add page contact

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,11 @@
+
+
+
+
+
 $(function () {
+   
+
    $('select').styler();
 
    $('.reviews__inner').slick({
@@ -9,4 +16,44 @@ $(function () {
       arrows: false,
 
    });
+   $(document).scroll(function () {
+      toggleHeader();
+   })
+
+   $(document).ready(function () {
+      $(function () { objectFitImages() });
+      jQuery('.menu__btn').click(function (e) {
+         e.preventDefault();
+         jQuery('.menu__btn-icon').toggleClass('opened');
+      });
+      $('.menu__btn').click(function (e) {
+         e.preventDefault();
+         $('.gadgets-nav__wrap').slideToggle(300);
+      });
+
+
+
+      $('.menu__btn').click(function (e) {
+         e.preventDefault();
+         $('.menu__btn').toggleClass('opened');
+      });
+   });
 });
+
+
+
+
+
+
+
+// function scrollFunction() {
+//    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//       logo.style.cssText = `width:100px;`;
+//       header.style.cssText = `position: fixed; background: rgba(0, 0, 0, 0.6);
+//       `;
+//    } else {
+//       logo.style.cssText = `width:;`;
+//       header.style.cssText = `position: absolute; background:;`;
+//    }
+// }
+
